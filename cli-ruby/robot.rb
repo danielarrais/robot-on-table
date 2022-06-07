7607c5ff223e25f86655c57092a40d9503a5e3d6
Daniel Arrais <danielarraiscarvalho@gmail.com>
2022-06-06 21:55:04 -0300

    Corrected sintax

--- cli-ruby/robot.rb
+++ cli-ruby/robot.rb
@@ -1,39 +1,35 @@
-NEXT_FACING = {
+CLOCKWISE_FLOW = {
     north: :east,
     east: :south,
     south: :west,
     west: :north
 }
 
-PREVIOUS_FACING = {
+ANTICLOCKWISE_FLOW = {
     north: :west,
     east: :north,
     south: :east,
     west: :south
 }
 
-class Robot {
+class Robot
     attr_reader :currentPosition;
     attr_reader :maxAreaToMove;
     attr_reader :facing;
     
     def initialize(attributes)
-        @currentPosition = attributes.initialPosition
-        @facing = attributes.facing.to_sym
-        @maxAreaToMove = attributes.maxAreaToMove
+        @currentPosition = attributes[:initialPosition]
+        @facing = attributes[:facing]
+        @maxAreaToMove = attributes[:maxAreaToMove]
     end
 
-    def left() {
-        @facing = NEXT_FACING[@facing]
+    def left()
+        @facing = CLOCKWISE_FLOW[@facing]
         puts "Rotated to #{@facing}"
-    }
+    end
 
-    def rigth() {
-        @facing = PREVIOUS_FACING[@facing]
+    def rigth()
+        @facing = ANTICLOCKWISE_FLOW[@facing]
         puts "Rotated to #{@facing}"
-    }
-
-    def move() {
-        
-    }
-}
+    end
+end
