c7b636ff3e2eefb408bbf589bf3e71e3889c4c3a
Daniel Arrais <danielarraiscarvalho@gmail.com>
2022-06-08 00:45:30 -0300

    Ajusted split lines and remove unecessary validations

--- cli-ruby/lib/input_commands_processor.rb
+++ cli-ruby/lib/input_commands_processor.rb
@@ -10,10 +10,10 @@ class InputCommandsProcessor
   def process
     return [] if @input.nil? || @input.empty?
 
-    commands_str = @input.split('\n')
-    valid_commands_str = validate_commands(commands_str)
+    commands = @input.split(/(\n)/)
+    valid_commands = validate_commands(commands)
 
-    build_commands(valid_commands_str)
+    build_commands(valid_commands)
   end
 
   private
@@ -21,20 +21,20 @@ class InputCommandsProcessor
   def validate_commands(commands)
     return [] if (commands.nil? || commands.empty?)
 
-    executable_commands = filter_commands_before_place(commands)
+    executable_commands = filter_commands_after_place(commands)
     executable_commands.select { |command| valid_command?(command) }
   end
 
-  def filter_commands_before_place(commands)
-    runnable_commands = []
+  def filter_commands_after_place(commands)
+    commands_filtered = []
 
     commands.each do |command|
-      if !runnable_commands.empty? || command.upcase.include?(PLACE_COMMAND_NAME)
-        runnable_commands.push(command)
+      if !commands.empty? || command.upcase.include?(PLACE_COMMAND_NAME)
+        commands_filtered.push(command)
       end
     end
 
-    runnable_commands
+    commands_filtered
   end
 
   def valid_command?(command)
@@ -48,12 +48,10 @@ class InputCommandsProcessor
   end
 
   def build_commands(valid_commands_str)
+    commands = []
+
     place_command_str = valid_commands_str.shift
     place_command = build_place_command(place_command_str)
-
-    return [] if (place_command.nil?)
-
-    commands = []
     commands.push(place_command)
 
     valid_commands_str.each do |command_str|
@@ -64,16 +62,13 @@ class InputCommandsProcessor
     commands
   end
 
-  def build_place_command(place_command_str)
-    return nil if (place_command_str.nil? || place_command_str.empty?)
-    return nil if (!place_command_str.include?(' '))
-
-    parts_of_place_command = place_command_str.split(' ')
-    command_name = parts_of_place_command[0]
-    params = parts_of_place_command[1].split(',')
+  def build_place_command(command)
+    parts_of_command = command.split(' ')
+    name = parts_of_command[0]
+    params = parts_of_command[1].split(',')
 
     {
-      name: command_name.to_sym,
+      name: name.to_sym,
       params: {
         x: params[0].to_i,
         y: params[1].to_i,
