fc225e2c9b7a45247ef138611d40a0e7800cc772
danielarrais <daniel.carvalho@a.catolica-to.edu.br>
2022-06-08 23:04:53 -0300

    Created unit testes for table game spec

---
+++ cli-ruby/spec/table_game_spec.rb
@@ -0,0 +1,67 @@
+require_relative '../lib/table_game'
+
+describe TableGame do
+  describe "Execute game" do
+    before do
+      @table_game = TableGame.new([5,5])
+    end
+
+    it "ignore commands before PLACE" do
+      commands = <<~HEREDOC
+        MOVE
+        RIGHT
+        RIGHT
+        RIGHT
+        LEFT
+        RIGHT
+        PLACE 4,4,NORTH
+        MOVE
+        RIGHT
+        REPORT
+      HEREDOC
+
+      expect do
+        @table_game.execute(commands)
+      end.to output('4,3,EAST').to_stdout
+    end
+
+    it "ignore commands when position of PLACE command is invalid" do
+      commands = <<~HEREDOC
+        PLACE 5,5,NORTH
+        MOVE
+        RIGHT
+        REPORT
+      HEREDOC
+
+      expect do
+        @table_game.execute(commands)
+      end.to_not output.to_stdout
+    end
+
+    it "ignore commands when facing of PLACE command is invalid" do
+      commands = <<~HEREDOC
+        PLACE 4,4,NORTH2
+        MOVE
+        RIGHT
+        REPORT
+      HEREDOC
+
+      expect do
+        @table_game.execute(commands)
+      end.to_not output.to_stdout
+    end
+
+    it "ignore commands when having different case" do
+      commands = <<~HEREDOC
+        PLaCE 4,4,NoRTH
+        MoVE
+        RIGhT
+        REPoRT
+      HEREDOC
+
+      expect do
+        @table_game.execute(commands)
+      end.to output('4,3,EAST').to_stdout
+    end
+  end
+end
