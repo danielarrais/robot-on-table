a95dd2daaa72f1c01b21c1ac2baa1821d9a3d254
Daniel Arrais <danielarraiscarvalho@gmail.com>
2022-06-24 16:56:39 -0300

    Fixed general bugs

--- web-app/src/constants.js
+++ web-app/src/constants.js
@@ -13,13 +13,13 @@ const FACINGS = {
     west: 'WEST'
 }
 
-const PLACE_COMMAND_REGEX = `/(\b${COMMANDS.place}\b) \d+,\d+,((\b${FACINGS.north}\b)|(\b${FACINGS.east}\b)|(\b${FACINGS.north}\b)|(\b${FACINGS.west}\b))/`;
-const OTHER_COMMANDS_REGEX = `/(\b${COMMANDS.right}\b)|(\b${COMMANDS.move}\b)|(\b${COMMANDS.left}\b)|(\b${COMMANDS.report}\b)/`;
+const PLACE_COMMAND_REGEX = new RegExp(`(\\b${COMMANDS.place}\\b) \\d+,\\d+,((\\b${FACINGS.north}\\b)|(\\b${FACINGS.east}\\b)|(\\b${FACINGS.north}\\b)|(\\b${FACINGS.west}\\b))`);
+const OTHER_COMMANDS_REGEX = new RegExp(`(\\b${COMMANDS.right}\\b)|(\\b${COMMANDS.move}\\b)|(\\b${COMMANDS.left}\\b)|(\\b${COMMANDS.report}\\b)`);
 const COMMANDS_REGEX_VALIDATION = {
     [COMMANDS.place]: PLACE_COMMAND_REGEX,
     [COMMANDS.right]: OTHER_COMMANDS_REGEX,
     [COMMANDS.left]: OTHER_COMMANDS_REGEX,
-    [COMMANDS.right]: OTHER_COMMANDS_REGEX,
+    [COMMANDS.move]: OTHER_COMMANDS_REGEX,
     [COMMANDS.report]: OTHER_COMMANDS_REGEX,
 };
 
